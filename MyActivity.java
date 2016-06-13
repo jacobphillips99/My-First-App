582b7cffe59dc0ff37532cd186eba7d78128e46d
jacobphillips99 <jacob.phillips8905@gmail.com>
2016-06-13 13:54:03 -0400

    MyActivity.java
    keeps track of the main java code

---
+++ MyActivity.java
@@ -0,0 +1,18 @@
+package com.example.jacob.myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+
+    public void sendMessage (View view) {
+        //do something in response to button
+    }
+}
